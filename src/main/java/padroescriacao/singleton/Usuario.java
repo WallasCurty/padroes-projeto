b1d6ed5d105562b2a2a09022c89716c772752533
Wallas Curty <65136877+WallasCurty@users.noreply.github.com>
2023-03-07 11:58:14 -0300

    Test
    Test Usuario

--- src/main/java/padroescriacao/singleton/Usuario.java
+++ src/main/java/padroescriacao/singleton/Usuario.java
@@ -21,6 +21,4 @@ public class Usuario {
         return uniqueInstance;
     }
 
-
-
 }
---
+++ src/test/java/padroescriacao/singleton/UsuarioTest.java
@@ -0,0 +1,13 @@
+package padroescriacao.singleton;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UsuarioTest {
+    @Test
+    public void enviarEmail(final String email){
+        System.out.println("Olá, '+nome+', tudo bem? Saja bem vindo");
+    }
+
+}

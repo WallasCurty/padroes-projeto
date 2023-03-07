1f2f74d6a595c28740ceb9e2e475fdf40b5dca96
Wallas Curty <65136877+WallasCurty@users.noreply.github.com>
2023-03-07 11:09:31 -0300

    Atributos
    Declaração de Atributos

--- src/main/java/padroescriacao/singleton/Usuario.java
+++ src/main/java/padroescriacao/singleton/Usuario.java
@@ -1,5 +1,9 @@
 package padroescriacao.singleton;
 
 public class Usuario {
-    
+    private static String nome;
+    private static String cpf;
+    private static String grupo;
+    private static String email;
+    private static Usuario uniqueInstance;
 }

c4f644554b73a91436bc2dbf7042429a3d083197
Wallas Curty <65136877+WallasCurty@users.noreply.github.com>
2023-03-07 11:16:47 -0300

    Construtor Usuario
    Tornando o construtor de Usuario como privado

--- src/main/java/padroescriacao/singleton/Usuario.java
+++ src/main/java/padroescriacao/singleton/Usuario.java
@@ -3,7 +3,17 @@ package padroescriacao.singleton;
 public class Usuario {
     private static String nome;
     private static String cpf;
-    private static String grupo;
+    private static String tipoUsuario;    //adm ou user
     private static String email;
     private static Usuario uniqueInstance;
+
+    private Usuario(String nome,String cpf,String tipoUsuario,String email){
+        Usuario.nome = nome;
+        Usuario.cpf = cpf;
+        Usuario.tipoUsuario = tipoUsuario;
+        Usuario.email = email;
+    }
+
+
+
 }
